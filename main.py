8c825eaf84e2185f3df5349612bc6bb7d96b3658
rohan.nishant1999 <rohan.nishant1999@gmail.com>
2021-06-26 03:13:20 +0530

    Add Data Testing and Output

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def knn(train, test, k=5):
         d = distance(test, ix)
         dist.append([d, iy])
 
-    dk = sorted(dist, key=lambda x: x[0])[:k]
-    labels = np.array(dk)[:, -1]
-    output = np.unique(labels, return_counts=True)
+    dk = sorted(dist, key=lambda X: X[0])[:k]
+    label = np.array(dk)[:, -1]
+    output = np.unique(label, return_counts=True)
     index = np.argmax(output[1])
     return output[0][index]
 
@@ -33,6 +33,7 @@ names = {}
 
 for fx in os.listdir(dataset_path):
     if fx.endswith('.npy'):
+        names[class_id] = fx[:-4]
         print("Loaded File: " + fx)
         data_item = np.load(dataset_path + fx)
         face_data.append(data_item)
@@ -50,47 +51,27 @@ trainset = np.concatenate((face_dataset, face_labels), axis=1)
 # print(trainset.shape)
 
 
+while True:
+    ret, frame = cap.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if not ret:
+        continue
+    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+    for face in faces:
+        x, y, w, h = face
+        offset = 10
+        face_section = frame[y - offset:y + h + offset, x - offset:x + w + offset]
+        face_section = cv2.resize(face_section, (100, 100))
 
+        out = knn(trainset, face_section.flatten())
+        predictedName = names[int(out)]
+        cv2.putText(frame, predictedName, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 0, 255), 2)
 
+    cv2.imshow("Faces", frame)
+    key_pressed = cv2.waitKey(1) & 0xFF
+    if key_pressed == ord('q'):
+        break
 
-
-
-
-
-
-
-
-
-
-#
-# while True:
-#     ret, frame = cap.read()
-#     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     if not ret:
-#         continue
-#
-#     faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
-#     faces = sorted(faces, key=lambda f: f[2] * f[3])
-#     face_section = frame
-#     for (x, y, w, h) in faces[-1:]:
-#         cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 0, 255), 2)
-#         offset = 10
-#         face_section = frame[y - offset:y + h + offset, x - offset:x + w + offset]
-#         face_section = cv2.resize(face_section, (100, 100))
-#         skip += 1
-#         if skip % 10 == 0:
-#             face_data.append(face_section)
-#             print(len(face_data))
-#     cv2.imshow("Video Frame", frame)
-#     cv2.imshow("Face Section", face_section)
-#     key_pressed = cv2.waitKey(1) & 0xFF
-#     if key_pressed == ord('q'):
-#         break
-#
-# face_data = np.asarray(face_data)
-# face_data = face_data.reshape((face_data.shape[0], -1))
-# print(face_data.shape)
-# np.save(dataset_path + file_name + '.npy', face_data)
-# print("Data Successfully saved")
-# cap.release()
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
